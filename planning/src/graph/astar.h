cb0225280d3bea67f89f270bac158e0eeefcecb6
rdu <ruixiang.du@gmail.com>
2016-01-19 21:37:06 -0500

    minor updates

--- planning/src/graph/astar.h
+++ planning/src/graph/astar.h
@@ -8,6 +8,18 @@
 #ifndef SRC_GRAPH_ASTAR_H_
 #define SRC_GRAPH_ASTAR_H_
 
+namespace srcl_ctrl {
 
+template<typename SearchGraph>
+class AStarSearch{
+public:
+	AStarSearch(){};
+	~AStarSearch(){};
+
+public:
+
+};
+
+}
 
 #endif /* SRC_GRAPH_ASTAR_H_ */
