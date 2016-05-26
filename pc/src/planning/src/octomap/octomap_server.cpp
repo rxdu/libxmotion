eb5abfdf8fef886cc6cf72c2bbbff36402d372a4
rdu <ruixiang.du@gmail.com>
2016-05-26 18:04:59 -0400

    started working on building octomap from point cloud

---
+++ pc/src/planning/src/octomap/octomap_server.cpp
@@ -0,0 +1,10 @@
+/*
+ * octomap_server.cpp
+ *
+ *  Created on: May 26, 2016
+ *      Author: rdu
+ */
+
+
+
+
---
+++ pc/src/planning/src/octomap/octomap_server.h
@@ -0,0 +1,24 @@
+/*
+ * octomap_server.h
+ *
+ *  Created on: May 26, 2016
+ *      Author: rdu
+ */
+
+#ifndef SRC_PLANNING_SRC_OCTOMAP_OCTOMAP_SERVER_H_
+#define SRC_PLANNING_SRC_OCTOMAP_OCTOMAP_SERVER_H_
+
+namespace srcl_ctrl {
+
+class OctomapServer {
+public:
+	OctomapServer();
+	~OctomapServer();
+
+
+};
+
+}
+
+
+#endif /* SRC_PLANNING_SRC_OCTOMAP_OCTOMAP_SERVER_H_ */
