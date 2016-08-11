19e148a1eb0afa9453e1cc355b16df800fe7b77b
rdu <ruixiang.du@gmail.com>
2016-08-11 17:42:43 -0400

    added reference code

--- matlab/qp_traj.m
+++ matlab/qp_traj.m
@@ -1,7 +1,17 @@
-% define two points
+close all;
+clear;
+clc;
+
+% define 3 points
 start = [0; 0; 0; 0 ]
+middle = [0.2; 0.6; 0.8; pi/8]
 goal = [1.0; 1.0; 1.0; pi/4]
 
-% try to find polynomial coefficients
+% plot points
+px = [start(1), middle(1), goal(1)]
+py = [start(2), middle(2), goal(2)]
+pz = [start(3), middle(3), goal(3)]
+
+plot3(px,py,pz, '-*')
 
 
