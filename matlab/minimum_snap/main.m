f5538b2a0e324604eb5abdcd2c2a3e6443505de8
rdu <ruixiang.du@gmail.com>
2016-08-12 17:30:23 -0400

    saved work

--- matlab/minimum_snap/main.m
+++ matlab/minimum_snap/main.m
@@ -20,42 +20,42 @@ clc
 
 %%%
 % set up problem
-% r = 6; %derivative to minimize in cost function
-% n = 11; %order of desired trajectory
-% m = 3; %number of pieces in trajectory
-% d = 2; %dimensions
-% 
-% % specify the m+1 keyframes
-% tDes = [0; 2; 4; 6]; %specify desired arrival times at keyframes
-% % specify desired positions and/or derivatives at keyframes, 
-% % Inf represents unconstrained values
-% % r x (m+1) x d, where each row i is the value the (i-1)th derivative of keyframe j for dimensions k 
-% posDes = zeros(r, m+1, d);
-% posDes(:, :, 1) = [0 1 1 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
-% posDes(:, :, 2) = [0 3 2 2; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
-% posDes(:, :, 3) = [1 2 3 4; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
-% [i, j, k] = size(posDes);
-% l = length(tDes);
-
 r = 6; %derivative to minimize in cost function
 n = 11; %order of desired trajectory
-m = 4; %number of pieces in trajectory
-d = 1; %dimensions
+m = 3; %number of pieces in trajectory
+d = 3; %dimensions
 
 % specify the m+1 keyframes
-tDes = [0;1.2; 3; 5; 6]; % %specify desired arrival times at keyframes
+tDes = [0; 2; 4; 6]; %specify desired arrival times at keyframes
 % specify desired positions and/or derivatives at keyframes, 
 % Inf represents unconstrained values
 % r x (m+1) x d, where each row i is the value the (i-1)th derivative of keyframe j for dimensions k 
-%posDes = zeros(r, m+1, d);
-% posDes(:, :, 1) = [0 1 1 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
-% posDes(:, :, 2) = [0 3 2 2; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
-% posDes(:, :, 3) = [1 2 3 4; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
-
-%posDes(:, :, 1) = [0 1 2 3; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
-posDes(:, :, 1) = [-0.1653 0.2194 0.3734 1 1.4; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0];
+posDes = zeros(r, m+1, d);
+posDes(:, :, 1) = [0 1 1 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
+posDes(:, :, 2) = [0 3 2 2; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
+posDes(:, :, 3) = [1 2 3 4; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
 [i, j, k] = size(posDes);
 l = length(tDes);
+
+% r = 6; %derivative to minimize in cost function
+% n = 11; %order of desired trajectory
+% m = 4; %number of pieces in trajectory
+% d = 1; %dimensions
+% 
+% % specify the m+1 keyframes
+% tDes = [0;1.2; 3; 5; 6]; % %specify desired arrival times at keyframes
+% % specify desired positions and/or derivatives at keyframes, 
+% % Inf represents unconstrained values
+% % r x (m+1) x d, where each row i is the value the (i-1)th derivative of keyframe j for dimensions k 
+% %posDes = zeros(r, m+1, d);
+% % posDes(:, :, 1) = [0 1 1 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
+% % posDes(:, :, 2) = [0 3 2 2; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
+% % posDes(:, :, 3) = [1 2 3 4; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0];
+% 
+% %posDes(:, :, 1) = [0 1 2 3; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0; 0 Inf Inf 0]; 
+% posDes(:, :, 1) = [-0.1653 0.2194 0.3734 1 1.4; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0; 0 Inf Inf Inf 0];
+% [i, j, k] = size(posDes);
+% l = length(tDes);
 
 % specify s corridor constraints
 ineqConst.numConst = 0; %integer, number of constraints 
@@ -113,48 +113,45 @@ end
 
 % %%%
 % % plot spline
-pp = spline(tDes(1:m+1, 1)', [posDes(1, 1:m+1)]);
-[breaks,coefs,l,k,d] = unmkpp(pp)
-
-
-
-
-tPlot = 0:0.1:tDes(m+1, 1);
-    this_der = pp;
-
-for j = 1:3*r,
-    figure()
-    hold on;
-    plot(tPlot, ppval(this_der,tPlot), '--');
-    %plot(tPlot, ppval(this_derxT,tPlot));
-    if j == 1,
-        plot(tDes(1:m+1, 1), posDes(1, 1:m+1)', 'o');
-    end
-    %legend('spline', 'optimization')
-    title(['derivative ' int2str(j-1)]);
-    
-    v = ppval(this_der, tDes(1:m+1, 1))
-
-    
-    p_der=fnder(this_der,1);
-
-    
-    this_der = p_der;
-
-    
-
-    
-end
-
-
-
+% pp = spline(tDes(1:m+1, 1)', [posDes(1, 1:m+1)]);
+% [breaks,coefs,l,k,d] = unmkpp(pp)
+% 
+% 
+% 
+% 
+% tPlot = 0:0.1:tDes(m+1, 1);
+%     this_der = pp;
+% 
+% for j = 1:3*r,
+%     figure()
+%     hold on;
+%     plot(tPlot, ppval(this_der,tPlot), '--');
+%     %plot(tPlot, ppval(this_derxT,tPlot));
+%     if j == 1,
+%         plot(tDes(1:m+1, 1), posDes(1, 1:m+1)', 'o');
+%     end
+%     %legend('spline', 'optimization')
+%     title(['derivative ' int2str(j-1)]);
+%     
+%     v = ppval(this_der, tDes(1:m+1, 1))
+% 
+%     
+%     p_der=fnder(this_der,1);
+% 
+%     
+%     this_der = p_der;
+% 
+%     
+% 
+%     
+% end
 
 %%%
-% plot QP traj
+plot QP traj
 dimLabels{1} = 'x (m)';
 dimLabels{2} = 'y (m)'; 
 dimLabels{3} = 'z (m)'; 
-%plotTraj(0, tDes(m+1), xT, n, m, d, tDes, posDes, 0.01, dimLabels, [], 2*r);
+plotTraj(0, tDes(m+1), xT, n, m, d, tDes, posDes, 0.01, dimLabels, [], 2*r);
 
 %xT
 %xT2
@@ -167,8 +164,6 @@ dimLabels{3} = 'z (m)';
 % 
 % end
 
-
-
 % % %%% 
 % % % plot the trajectory
 % % 
@@ -179,9 +174,9 @@ dimLabels{3} = 'z (m)';
 % plotDim = []; %[1 2]; %if you want to plot two dimensions against each other, specify here 
 %     % nxm matrix, creates n plots of column 1 vs. column 2
 %     
-%plotTraj(0, tDes(m+1), xT, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, 2*r);
-%plotTraj(0, tDes(m+1), xT2, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, r);
-%plotTraj(0, tDes(m+1), xT3, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, r);
+% plotTraj(0, tDes(m+1), xT, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, 2*r);
+% plotTraj(0, tDes(m+1), xT2, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, r);
+% plotTraj(0, tDes(m+1), xT3, n, m, d, tDes, posDes, 0.01, dimLabels, plotDim, r);
 
 
 
