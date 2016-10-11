4465d8b462635ea8b8cda93cedd5e2c6c4fd26e9
rdu <ruixiang.du@gmail.com>
2016-10-10 23:27:48 -0400

    added a script to test telemetry2 of pixracer

---
+++ mcu/python/test_connection.py
@@ -0,0 +1,57 @@
+# import the necessary packages
+import argparse
+import cv2
+import numpy as np
+import os
+import time
+from dronekit import *
+import droneapi
+import socket
+import sys
+
+def main():
+    # Import DroneKit-Python
+    from dronekit import connect, VehicleMode
+
+    # Connect to the Vehicle.
+    #print("Connecting to vehicle on: %s" % (connection_string,))
+    vehicle = connect('/dev/ttyUSB0', baud = 57600)
+
+    # Get some vehicle attributes (state)
+    print "\nGet some vehicle attribute values:"
+    print " Autopilot Firmware version: %s" % vehicle.version
+    print "   Major version number: %s" % vehicle.version.major
+    print "   Minor version number: %s" % vehicle.version.minor
+    print "   Patch version number: %s" % vehicle.version.patch
+    print "   Release type: %s" % vehicle.version.release_type()
+    print "   Release version: %s" % vehicle.version.release_version()
+    print "   Stable release?: %s" % vehicle.version.is_stable()
+
+    print " Attitude: %s" % vehicle.attitude
+    print " Velocity: %s" % vehicle.velocity
+    print " GPS: %s" % vehicle.gps_0
+    print " Gimbal status: %s" % vehicle.gimbal
+    print " Battery: %s" % vehicle.battery
+    print " EKF OK?: %s" % vehicle.ekf_ok
+    print " Last Heartbeat: %s" % vehicle.last_heartbeat
+    print " Rangefinder: %s" % vehicle.rangefinder
+    print " Rangefinder distance: %s" % vehicle.rangefinder.distance
+    print " Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+    print " Heading: %s" % vehicle.heading
+    print " Is Armable?: %s" % vehicle.is_armable
+    #print " System status: %s" % vehicle.system_status.state
+    print " Groundspeed: %s" % vehicle.groundspeed    # settable
+    print " Airspeed: %s" % vehicle.airspeed    # settable
+    print " Mode: %s" % vehicle.mode.name    # settable
+    print " Armed: %s" % vehicle.armed    # settable
+
+    # Close vehicle object before exiting script
+    vehicle.close()
+
+    # Shut down simulator
+    print("Completed")
+
+    return
+
+if __name__ == "__main__":
+    main()
