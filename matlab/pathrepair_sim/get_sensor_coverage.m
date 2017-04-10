bccfabd2ec9e595efb5d9afeb8e48c6880f2f484
rdu <ruixiang.du@gmail.com>
2017-04-10 17:03:36 -0400

    saved work

---
+++ matlab/pathrepair_sim/get_sensor_coverage.m
@@ -0,0 +1,9 @@
+function sensor_coverage = get_sensor_coverage(space, pos, heading)
+    sensor_range = 5;
+    
+    search_finished = false;
+    
+    while not search_finished
+        
+    end        
+end
