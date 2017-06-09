ff8ca323c8621f6d4c3486ab9322dd690985bad4
rdu <ruixiang.du@gmail.com>
2017-06-08 22:37:45 -0400

    updated eigen to 3.3.3, seperated dec and def in common module

--- pc/common/poly_curve.h
+++ pc/common/poly_curve.h
@@ -26,27 +26,9 @@ typedef struct {
 
 class PolyCurve {
 public:
-	PolyCurve():is_nondim_(false){}
-
-	PolyCurve(const std::vector<double>& coefficients, bool coeff_nondim, double start_t, double end_t):
-			is_nondim_(coeff_nondim)
-	{
-		for(auto& coeff:coefficients)
-			param_.coeffs.push_back(coeff);
-		param_.ts = start_t;
-		param_.te = end_t;
-	}
-
-	PolyCurve(const std::vector<double>& coefficients, bool coeff_nondim, double start_t, double end_t, std::string str):
-			is_nondim_(coeff_nondim)
-	{
-		for(auto& coeff:coefficients)
-			param_.coeffs.push_back(coeff);
-		param_.ts = start_t;
-		param_.te = end_t;
-		name_ = str;
-	}
-
+	PolyCurve():is_nondim_(false){};
+	PolyCurve(const std::vector<double>& coefficients, bool coeff_nondim, double start_t, double end_t);
+	PolyCurve(const std::vector<double>& coefficients, bool coeff_nondim, double start_t, double end_t, std::string str);
 	~PolyCurve()=default;
 
 private:
@@ -57,78 +39,21 @@ public:
 	std::string name_;
 
 private:
-	double GetCurvePointDerivVal(uint32_t deriv, double t)
-	{
-		int64_t N = param_.coeffs.size() - 1;
-		int64_t r = deriv;
-		double ts = GetRefactoredTime(t);
-		PolyHelper::PolynomialCoeffs deriv_coeff(N + 1);
-		PolyHelper::GetDerivativeCoeffs(N, r, deriv_coeff);
-
-		double val = 0;
-		for(int i = 0; i <= N; i++)
-		{
-			double item_val;
-
-			if(N - i >= r) {
-				item_val =  deriv_coeff[i] * param_.coeffs[i] * std::pow(ts, N - i - r);
-				//std::cout << "deriv_coeff: " << deriv_coeff[i] << " ; coeff: " << coeffs_[i] << " ; power: " << N-i-r << " ; val: " << item_val << std::endl;
-			}
-			else
-				item_val = 0;
-
-			val += item_val;
-		}
-
-		return val;
-	}
+	double GetCurvePointDerivVal(uint32_t deriv, double t);
 
 public:
 	void SetCurveName(std::string str){ name_ = str; };
 	std::string GetCurveName(){ return name_; };
 
-	double GetRefactoredTime(double t)
-	{
-		if(t < param_.ts)
-			t = param_.ts;
-		if(t > param_.te)
-			t = param_.te;
+	double GetRefactoredTime(double t);
 
-		if(is_nondim_)
-			return (t - param_.ts) / (param_.te - param_.ts);
-		else
-			return t;
-	}
+	double GetCurvePointPos(double t);
 
-	double GetCurvePointPos(double t)
-	{
-		double ts = GetRefactoredTime(t);
+	double GetCurvePointVel(double t);
 
-		return GetCurvePointDerivVal(0, ts);
-	}
+	double GetCurvePointAcc(double t);
 
-	double GetCurvePointVel(double t)
-	{
-		double ts = GetRefactoredTime(t);
-
-		return GetCurvePointDerivVal(1, ts);
-	}
-
-	double GetCurvePointAcc(double t)
-	{
-		double ts = GetRefactoredTime(t);
-
-		return GetCurvePointDerivVal(2, ts);
-	}
-
-	void print()
-	{
-		uint32_t coeff_idx = 0;
-		std::cout << "\ncurve " << name_ << std::endl;
-		for(auto& coef:param_.coeffs)
-			std::cout << coeff_idx++ << " : " << coef << std::endl;
-		std::cout << "start time: " << param_.ts << " , end time: " << param_.te << std::endl;
-	}
+	void print();
 };
 
 }
--- pc/common/poly_helper.h
+++ pc/common/poly_helper.h
@@ -10,6 +10,7 @@
 
 #include <cstdint>
 #include <cmath>
+#include <vector>
 
 #include "eigen3/Eigen/Core"
 
@@ -20,64 +21,8 @@ namespace PolyHelper
 
 using PolynomialCoeffs = Eigen::Array<float,1, Eigen::Dynamic>;
 
-static void inline GetDerivativeCoeffs(uint32_t poly_order, uint32_t deriv_order, Eigen::Ref<PolynomialCoeffs> coeffs)
-{
-	{
-		int64_t N = poly_order;
-		int64_t r = deriv_order;
-
-		coeffs = Eigen::ArrayXXf::Ones(1, N+1);
-
-		/* if deriv_order == 0, no derivative is taken */
-		if(r == 0)
-			return;
-
-		/* otherwise calculate derivative coefficients */
-		int64_t n;
-		for(n = 0; n <= N; n++)
-		{
-			if(n < r)
-				coeffs(0, N - n) = 0;
-			else
-			{
-				uint64_t multiply = 1;
-				for(uint64_t m = 0; m <= r - 1; m++)
-				{
-					multiply *= (n - m);
-				}
-				coeffs(0,N - n) = multiply;
-			}
-		}
-	}
-}
-
-double inline GetPolynomialValue(std::vector<double> coeffs, uint32_t deriv_order, double tau)
-{
-	double val = 0;
-	int64_t N = coeffs.size() - 1;
-	int64_t r = deriv_order;
-
-	PolynomialCoeffs deriv_coeff(N + 1);
-	GetDerivativeCoeffs(N, r, deriv_coeff);
-
-	uint32_t coeff_size = coeffs.size();
-	for(int i = 0; i <= N; i++)
-	{
-		double item_val;
-
-		if(N - i >= r) {
-			item_val =  deriv_coeff[i] * coeffs[i] * std::pow(tau, N - i - r);
-
-			//std::cout << "deriv_coeff: " << deriv_coeff[i] << " ; coeff: " << coeffs[i] << " ; power: " << N-i-r << " ; val: " << item_val << std::endl;
-		}
-		else
-			item_val = 0;
-
-		val += item_val;
-	}
-
-	return val;
-}
+void GetDerivativeCoeffs(uint32_t poly_order, uint32_t deriv_order, Eigen::Ref<PolynomialCoeffs> coeffs);
+double GetPolynomialValue(std::vector<double> coeffs, uint32_t deriv_order, double tau);
 
 }
 }
