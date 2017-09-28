7f81748d2347e8378776052473c46ad78204c10e
rdu <ruixiang.du@gmail.com>
2017-09-28 16:11:51 -0400

    minor changes

--- matlab/envsim/plot_envsim_data.m
+++ matlab/envsim/plot_envsim_data.m
@@ -1,12 +1,32 @@
 % script to plot simulation data
 close all; clear; clc;
 
-data1 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/prsim.20170927074826.5-45percent.data');
-data2 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/prsim.20170926073337.8-45percent.data');
-data3 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/prsim.20170927092831-12-45percent.data');
+data1 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/batch1/prsim.20170927074826.5-45percent.data');
+data2 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/batch1/prsim.20170926073337.8-45percent.data');
+data3 = csvread('/home/rdu/Workspace/librav/data/log/quad/prsim/batch1/prsim.20170927092831-12-45percent.data');
+
+res_range5 = data1(1:500,5);
+res_range8 = data2(1:500,5);
+res_range12 = data3(160:659,5);
 
 res_mean = [mean(data1(:,5)) mean(data2(:,5)) mean(data3(:,5))]
 res_max = [max(data1(:,5)) max(data2(:,5)) max(data3(:,5))]
+
+cnt_range5 = 0;
+cnt_range8 = 0;
+cnt_range12 = 0;
+threshold = 0.1;
+for i = 1:500
+    if res_range5(i) > threshold
+        cnt_range5 = cnt_range5 + 1;
+    end
+    if res_range8(i) > threshold
+        cnt_range8 = cnt_range8 + 1;
+    end
+    if res_range12(i) > threshold
+        cnt_range12 = cnt_range12 + 1;
+    end
+end
 
 figure
 plot(data1(1:500,5),'-d')
