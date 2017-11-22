211ee3667c58974ca23d5658e0a265bf61997ba8
Ruixiang Du <ruixiang.du@gmail.com>
2017-11-22 17:41:00 -0500

    added a script to show log data

--- matlab/rccar/disp_log.m
+++ matlab/rccar/disp_log.m
@@ -11,14 +11,12 @@ close all;clear;clc;
 
 imu_log_file = '/home/rdu/CarLog/20171122/car/raw_imu.20171122171936.data'
 mag_log_file = '/home/rdu/CarLog/20171122/car/raw_mag.20171122171936.data'
-spd_log_file = '/home/rdu/CarLog/20171122/car/raw_spd.20171122171936.data'
 
 %imu_log_file = '/home/rdu/CarLog/raw_imu.20171112000855.data'
 %mag_log_file = '/home/rdu/CarLog/raw_mag.20171112000855.data'
 
 imu_data = csvread(imu_log_file);
 mag_data = csvread(mag_log_file);
-spd_data = csvread(spd_log_file);
 
 t_imu = imu_data(:,1);
 tb_imu = t_imu(1);
@@ -27,10 +25,6 @@ t_imu = t_imu - tb_imu;
 t_mag = mag_data(:,1);
 tb_mag = t_mag(1);
 t_mag = t_mag - tb_mag;
-
-t_spd = spd_data(:,1);
-tb_spd = t_spd(1);
-t_spd = t_spd - tb_spd;
 
 figure(1)
 hold on
@@ -55,10 +49,4 @@ plot(t_mag, mag_data(:,2),'r')
 plot(t_mag, mag_data(:,3),'g')
 plot(t_mag, mag_data(:,4),'b')
 legend('x','y','z')
-
-figure(4)
-hold on
-title('speed')
-plot(t_spd, spd_data(:,3),'r')
-legend('speed')
 
