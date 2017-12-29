d53a86cf954ca00a836874e29a34b60c9801406f
rdu <ruixiang.du@gmail.com>
2017-12-28 23:05:54 -0500

    minor fix to octomap lib: isnan() -> ::is_nan()

--- src/libraries/octomap-1.8.0/octomap/src/compare_octrees.cpp
+++ src/libraries/octomap-1.8.0/octomap/src/compare_octrees.cpp
@@ -132,7 +132,7 @@ int main(int argc, char** argv) {
       else
         kld +=log(p1/p2)*p1 + log((1-p1)/(1-p2))*(1-p1);
 
-      if (isnan(kld)){
+      if (::isnan(kld)){
         OCTOMAP_ERROR("KLD is nan! KLD(%f,%f)=%f; sum = %f", p1, p2, kld, kld_sum);
         exit(-1);
       }
