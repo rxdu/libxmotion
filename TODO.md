a86c384e01e58c2899110bba91451adcb1ec113e
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-12 16:16:34 -0400

    added todo list, added mathgl test

---
+++ TODO.md
@@ -0,0 +1,19 @@
+# TODO List
+
+## Planning
+
+- [ ] Iterators for Vertex_t and Edge_t
+- [ ] Priority queue that supports element priority update
+- [ ] RRT and RRT* with Dubins model  
+
+## Control
+
+- [ ] Kalman filter for RC Car
+
+## Simulation
+
+- [ ] Finish RC Car simulation interface
+
+## Visualization
+
+- [ ] Better surface visualization
--- src/fastplot/tests/test_graphplot.cpp
+++ src/fastplot/tests/test_graphplot.cpp
@@ -22,36 +22,6 @@ int main()
     FastPlot::CreateSquareGridLayer(grid, img);
     // FastPlot::PlotSquareGrid(grid);
 
-    // std::shared_ptr<Graph_t<SquareCell*>> graph = std::make_shared<Graph_t<SquareCell*>>();
-
-    // for(auto& cell_col : grid->grid_cells_)
-    //     for(auto& cell : cell_col)
-    //     {
-    //         uint64_t current_nodeid = cell->id_;
-    //         std::cout << "current id: " << current_nodeid << std::endl;
-
-    //         if(cell->occupancy_ == OccupancyType::FREE) {
-    //             auto neighbour_list = grid->GetNeighbours(current_nodeid,true);
-
-    //             for(auto& neighbour : neighbour_list)
-    //             {
-    //                 if(neighbour->occupancy_ == OccupancyType::FREE)
-    //                 {
-    //                     double error_x,error_y, cost = 0;
-    //                     error_x = std::abs(neighbour->position_.x - cell->position_.x);
-    //                     error_y = std::abs(neighbour->position_.y - cell->position_.y);
-    //                     cost = std::sqrt(error_x*error_x + error_y*error_y);
-
-    //                     graph->AddEdge(cell, neighbour, cost);
-    //                 }
-
-    //                 std::cout << " - neighbour id: " << neighbour->id_ << std::endl;
-    //             }
-    //         }
-    //     }
-
-    // std::cout << "graph vertex num: " << graph->GetGraphVertices().size() << " , edge num: " << graph->GetGraphEdges().size() << std::endl;
-
     FastPlot::AddGraphLayer(Planner::BuildGraphFromSquareGrid(grid), img, img, false);
     FastPlot::ShowImage(img);
 
