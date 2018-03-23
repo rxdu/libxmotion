a51a69191d89396ed36b1bf5bac46dbd01c71be2
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-23 18:29:22 -0400

    saved work

---
+++ src/utilities/image/image_io.cpp
@@ -0,0 +1,9 @@
+/* 
+ * image_io.cpp
+ * 
+ * Created on: Mar 23, 2018 17:34
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */ 
+
---
+++ src/utilities/image/image_io.hpp
@@ -0,0 +1,24 @@
+/* 
+ * image_io.hpp
+ * 
+ * Created on: Mar 23, 2018 17:27
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */
+
+#ifndef IMAGE_IO_HPP
+#define IMAGE_IO_HPP
+
+#include <dlib/image_io.h>
+#include <Eigen/Dense>
+
+namespace librav
+{
+namespace ImageIO
+{
+void DisplayImage();
+}
+}
+
+#endif /* IMAGE_IO_HPP */
