4ab479715ab796a8f963d97c95073ac4bb20156c
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-27 17:44:34 -0400

    updated graph, some test with glfw

---
+++ src/lightviz/include/lightviz/lightgui.hpp
@@ -0,0 +1,16 @@
+/* 
+ * lightgui.hpp
+ * 
+ * Created on: Mar 27, 2018 14:40
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */ 
+
+#ifndef LIGHTGUI_HPP
+#define LIGHTGUI_HPP
+
+#include <stdio.h>
+#include "imgui/imgui.h"
+
+#endif /* LIGHTGUI_HPP */
