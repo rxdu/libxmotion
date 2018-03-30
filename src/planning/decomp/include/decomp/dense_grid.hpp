2faab48bda94eb8df060fab0bce9b992f5c772a9
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-30 13:48:14 -0400

    verified grid viz

--- src/planning/decomp/include/decomp/dense_grid.hpp
+++ src/planning/decomp/include/decomp/dense_grid.hpp
@@ -34,20 +34,20 @@ namespace librav
 
 class DenseGrid : public GridBase<double>
 {
-  public:
-    DenseGrid(int32_t size_x, int32_t size_y);
-    ~DenseGrid() = default;
+public:
+  DenseGrid(int32_t size_x, int32_t size_y);
+  ~DenseGrid() = default;
 
-    void SetValueAtCoordinate(int64_t x, int64_t y, double val);
-    double GetValueAtCoordinate(int64_t x, int64_t y);
+  void SetValueAtCoordinate(int64_t x, int64_t y, double val);
+  double GetValueAtCoordinate(int64_t x, int64_t y);
 
-    // public functions inherited from GridBase
-    /****************************************************************
+  // public functions inherited from GridBase
+  /****************************************************************
     void ResizeGrid(int64_t x, int64_t y);
     void SetOriginCoordinate(int64_t origin_x, int64_t origin_y);   
     *****************************************************************/
 
-    Eigen::MatrixXd GetGridMatrix(bool normalize);
+  Eigen::MatrixXd GetGridMatrix(bool normalize = false);
 };
 }
 
