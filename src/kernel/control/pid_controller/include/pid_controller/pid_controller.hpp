a000e96a8f8f561dde5a8accccb3a08aa75b23f6
Ruixiang Du <ruixiang.du@gmail.com>
2018-11-22 11:16:32 -0500

    saved work

---
+++ src/kernel/control/pid_controller/include/pid_controller/pid_controller.hpp
@@ -0,0 +1,22 @@
+/* 
+ * pid_controller.hpp
+ * 
+ * Created on: Nov 22, 2018 11:11
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */
+
+#ifndef PID_CONTROLLER_HPP
+#define PID_CONTROLLER_HPP
+
+namespace librav
+{
+// PID controller for a SISO system
+class PIDController
+{
+  public:
+};
+} // namespace librav
+
+#endif /* PID_CONTROLLER_HPP */
