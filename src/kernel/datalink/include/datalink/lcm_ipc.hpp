53dbd9055df9818e8ab2f9187c14898f2c17e769
Ruixiang Du <ruixiang.du@gmail.com>
2018-11-22 08:56:00 -0500

    cleaned up lcm dependency

---
+++ src/kernel/datalink/include/datalink/lcm_ipc.hpp
@@ -0,0 +1,13 @@
+/* 
+ * lcm_ipc.hpp
+ * 
+ * Created on: Nov 22, 2018 07:32
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */ 
+
+#ifndef LCM_IPC_HPP
+#define LCM_IPC_HPP
+
+#endif /* LCM_IPC_HPP */
---
+++ src/kernel/datalink/src/lcm_ipc.cpp
@@ -0,0 +1,9 @@
+/* 
+ * lcm_ipc.cpp
+ * 
+ * Created on: Nov 22, 2018 07:52
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */ 
+
