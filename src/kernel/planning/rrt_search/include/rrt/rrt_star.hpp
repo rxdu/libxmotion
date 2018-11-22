bc799398da1fa30291a02307dcc8cd564f3325f8
Ruixiang Du <ruixiang.du@gmail.com>
2018-11-22 09:21:14 -0500

    minor

---
+++ src/kernel/planning/rrt_search/include/rrt/rrt_star.hpp
@@ -0,0 +1,13 @@
+/* 
+ * rrt_star.hpp
+ * 
+ * Created on: Nov 22, 2018 09:00
+ * Description: 
+ * 
+ * Copyright (c) 2018 Ruixiang Du (rdu)
+ */ 
+
+#ifndef RRT_STAR_HPP
+#define RRT_STAR_HPP
+
+#endif /* RRT_STAR_HPP */
