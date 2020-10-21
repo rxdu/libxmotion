8517d87f868a398ebc3f88fc8a541cf6825b547a
Ruixiang Du <ruixiang.du@gmail.com>
2020-10-21 23:24:47 +0800

    saved work on fastdraw

---
+++ src/modules/visualization/fastdraw/test/test_fshape.cpp
@@ -0,0 +1,18 @@
+/*
+ * test_fshape.cpp
+ *
+ * Created on: Oct 21, 2020 22:43
+ * Description:
+ *
+ * Copyright (c) 2020 Ruixiang Du (rdu)
+ */
+
+#include "fastdraw/fastdraw.hpp"
+
+using namespace ivnav;
+
+int main(int argc, char* argv[]) {
+  FastCanvas canvas(800, 450, "demo");
+  //   canvas.Demo();
+  canvas.Show();
+}
