4120f79b6b70dbf3a0091bc4e72140162b51f536
Ruixiang Du <ruixiang.du@gmail.com>
2020-10-24 10:39:13 +0800

    odeint work with bicycle model

--- src/modules/control/model/tests/test_propagator2.cpp
+++ src/modules/control/model/tests/test_propagator2.cpp
@@ -20,18 +20,18 @@ int main() {
   double tf = 10;
   double dt = 0.01;
 
-  BicycleKinematics::state_type x = {0.0, 8.0};
-  BicycleKinematics model({0.1, 0});
+  BicycleKinematics::state_type x = {0.0, 0.0, 0.0, 0.0};
+  BicycleKinematics model({0.8, 0});
 
-  //   boost::numeric::odeint::integrate_const(
-  //       boost::numeric::odeint::runge_kutta4<BicycleKinematics::state_type>(),
-  //       model, x, 0.0, 10.0, 0.01);
+  boost::numeric::odeint::integrate_const(
+      boost::numeric::odeint::runge_kutta4<BicycleKinematics::state_type>(),
+      model, x, 0.0, 10.0, 0.01);
 
-  runge_kutta4<std::vector<double>> rk4;
-  double t = t0;
-  for (size_t i = 0; i < 1000; ++i, t += dt) {
-    rk4.do_step(model, x, t, dt);
-  }
+  //   runge_kutta4<std::vector<double>> rk4;
+  //   double t = t0;
+  //   for (size_t i = 0; i < 1000; ++i, t += dt) {
+  //     rk4.do_step(model, x, t, dt);
+  //   }
 
   std::cout << "final state: " << x[0] << " , " << x[1] << std::endl;
 
