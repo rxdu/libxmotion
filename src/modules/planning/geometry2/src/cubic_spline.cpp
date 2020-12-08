47d04958537656dbeaf5380f98d927dff8200fe4
Ruixiang Du <ruixiang.du@gmail.com>
2020-12-08 23:42:47 +0800

    saved work on clamped cubic spline, memory issue found but not fixed yet

--- src/modules/planning/geometry2/src/cubic_spline.cpp
+++ src/modules/planning/geometry2/src/cubic_spline.cpp
@@ -33,7 +33,7 @@ void CubicSpline::Interpolate(const std::vector<Knot> &knots) {
   if (n < 2) return;
 
   Eigen::VectorXd x = Eigen::VectorXd::Zero(n + 1);
-  Eigen::VectorXd a = Eigen::VectorXd::Zero(n);
+  Eigen::VectorXd a = Eigen::VectorXd::Zero(n + 1);
   Eigen::VectorXd h = Eigen::VectorXd::Zero(n);
 
   for (size_t i = 0; i < knots_.size(); ++i) {
@@ -87,10 +87,65 @@ void CubicSpline::Interpolate(double fp0, double fpn,
   if (!knots.empty()) knots_ = knots;
 
   /* interpolation */
-  const uint32_t n = knots_.size();
+  const size_t n = knots_.size() - 1;
 
   // no interpolation if size < 3
-  if (n < 3) return;
+  if (n < 2) return;
+
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(n + 1);
+  Eigen::VectorXd a = Eigen::VectorXd::Zero(n + 1);
+  Eigen::VectorXd h = Eigen::VectorXd::Zero(n);
+
+  for (size_t i = 0; i < knots_.size(); ++i) {
+    x(i) = knots_[i].x();
+    a(i) = knots_[i].y();
+  }
+
+  for (size_t i = 0; i < n; ++i) {
+    h(i) = x(i + 1) - x(i);
+  }
+
+  Eigen::VectorXd alpha = Eigen::VectorXd::Zero(n + 1);
+  alpha(0) = 3 * (a(1) - a(0)) / h(0) - 3 * fp0;
+  alpha(n) = 3 * fpn - 3 * (a(n) - a(n - 1)) / h(n - 1);
+
+  for (size_t i = 1; i < n; ++i) {
+    alpha(i) = 3 / h(i) * (a(i + 1) - a(i)) - 3 / h(i - 1) * (a(i) - a(i - 1));
+  }
+
+  Eigen::VectorXd miu = Eigen::VectorXd::Zero(n + 1);
+  Eigen::VectorXd l = Eigen::VectorXd::Zero(n + 1);
+  Eigen::VectorXd z = Eigen::VectorXd::Zero(n + 1);
+
+  Eigen::VectorXd b = Eigen::VectorXd::Zero(n);
+  Eigen::VectorXd c = Eigen::VectorXd::Zero(n);
+  Eigen::VectorXd d = Eigen::VectorXd::Zero(n);
+
+  l(0) = 2 * h(0);
+  miu(0) = 0.5;
+  z(0) = alpha(0) / l(0);
+
+  for (size_t i = 1; i < n; ++i) {
+    l(i) = 2 * (x(i + 1) - x(i - 1)) - h(i - 1) * miu(i - 1);
+    miu(i) = h(i) / l(i);
+    z(i) = (alpha(i) - h(i - 1) * z(i - 1)) / l(i);
+  }
+  
+  l(n) = h(n - 1) * (2 - miu(n - 1));
+  z(n) = (alpha(n) - h(n - 1) * z(n - 1)) / l(n);
+  c(n) = z(n);
+
+  for (int64_t j = n - 1; j >= 0; --j) {
+    c(j) = z(j) - miu(j) * c(j + 1);
+    b(j) = (a(j + 1) - a(j)) / h(j) - h(j) * (c(j + 1) + 2 * c(j)) / 3;
+    d(j) = (c(j + 1) - c(j)) / (3 * h(j));
+  }
+
+  std::cout << "result: " << std::endl;
+  std::cout << "a: " << a.transpose() << std::endl;
+  std::cout << "b: " << b.transpose() << std::endl;
+  std::cout << "c: " << c.transpose() << std::endl;
+  std::cout << "d: " << d.transpose() << std::endl;
 }
 
 double CubicSpline::Evaluate(double x, uint32_t derivative) const {
--- src/modules/planning/geometry2/test/devel/test_cubic_spline.cpp
+++ src/modules/planning/geometry2/test/devel/test_cubic_spline.cpp
@@ -7,19 +7,33 @@
 using namespace robotnav;
 
 int main() {
-  std::vector<CubicSpline::Knot> knots;
+//   std::vector<CubicSpline::Knot> knots;
 
-  knots.emplace_back(1, 2);
-  knots.emplace_back(2, 3);
-  knots.emplace_back(3, 5);
+//   knots.emplace_back(1, 2);
+//   knots.emplace_back(2, 3);
+//   knots.emplace_back(3, 5);
 
-  std::cout << "knot size: " << knots.size() << std::endl;
+//   std::cout << "knot size: " << knots.size() << std::endl;
 
-  CubicSpline spline(knots);
-  
-//   for (double x = knots.front().x(); x < knots.back().x(); x += 0.01) {
-//     std::cout << x << " , " << spline.Evaluate(x) << std::endl;
-//   }
+//   CubicSpline spline(knots);
+
+  //   for (double x = knots.front().x(); x < knots.back().x(); x += 0.01) {
+  //     std::cout << x << " , " << spline.Evaluate(x) << std::endl;
+  //   }
+
+  std::cout << "---------------" << std::endl;
+
+  std::vector<CubicSpline::Knot> knots2;
+
+  knots2.emplace_back(0, 1);
+  knots2.emplace_back(1, std::exp(1));
+  knots2.emplace_back(2, std::exp(2));
+  knots2.emplace_back(3, std::exp(3));
+
+  std::cout << "knot size: " << knots2.size() << std::endl;
+
+  CubicSpline spline2;
+  spline2.Interpolate(1, std::exp(3), knots2);
 
   return 0;
 }
