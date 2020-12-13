6ccb9f28a47210a4e6b66d8a9e83642b084aae3b
Ruixiang Du <ruixiang.du@gmail.com>
2020-12-13 11:47:04 +0800

    code reformat for decomp module

--- src/modules/planning/decomp/include/decomp/details/rect_grid_base.hpp
+++ src/modules/planning/decomp/include/decomp/details/rect_grid_base.hpp
@@ -1,9 +1,9 @@
-/* 
+/*
  * rect_grid_base.hpp
- * 
+ *
  * Created on: Mar 28, 2018 23:34
  * Description: Base class for rectangular grid.
- * 
+ *
  * Copyright (c) 2018 Ruixiang Du (rdu)
  */
 
@@ -21,13 +21,10 @@
 
 #include "decomp/details/rect_grid_base_tiles.hpp"
 
-namespace robotnav
-{
-
 /*
- * 
+ *
  *  Grid Coordinate System:
- * 
+ *
  *              |
  *              |
  *              |
@@ -37,165 +34,146 @@ namespace robotnav
  *              |
  *              |
  *              |
- *              | 
+ *              |
  *              v
  *              y
- *                           
- * 
+ *
+ *
  *  Internal Coordinate System:
- *		
- *		o ------------------> x  
+ *
+ *		o ------------------> x
  *		|         |
  *		|         |
  *		|         |
  *		| --------o' (origin offset)
- *		|         
- *		|         
- *		|                 
+ *		|
+ *		|
+ *		|
  *		v
  *		y
- * 
+ *
  */
 
 ////////////////////////////////////////////////////////////////////
 
-class RectGridIndex
-{
-  public:
-    RectGridIndex() : index_x_(0), index_y_(0) {}
-    RectGridIndex(int64_t x = 0, int64_t y = 0) : index_x_(x), index_y_(y) {}
-    ~RectGridIndex() = default;
+namespace robotnav {
+class RectGridIndex {
+ public:
+  RectGridIndex(int64_t x = 0, int64_t y = 0) : index_x_(x), index_y_(y) {}
 
-    inline int64_t GetX() const { return index_x_; };
-    inline int64_t GetY() const { return index_y_; };
-    inline void SetX(int64_t x) { index_x_ = x; };
-    inline void SetY(int64_t y) { index_y_ = y; };
-    inline void SetXY(int64_t x, int64_t y)
-    {
-        index_x_ = x;
-        index_y_ = y;
-    };
+  inline int64_t GetX() const { return index_x_; };
+  inline int64_t GetY() const { return index_y_; };
+  inline void SetX(int64_t x) { index_x_ = x; };
+  inline void SetY(int64_t y) { index_y_ = y; };
+  inline void SetXY(int64_t x, int64_t y) {
+    index_x_ = x;
+    index_y_ = y;
+  };
 
-  private:
-    int64_t index_x_;
-    int64_t index_y_;
+ private:
+  int64_t index_x_ = 0;
+  int64_t index_y_ = 0;
 };
 
 ////////////////////////////////////////////////////////////////////
 
 template <typename TileType>
-class RectGridBase : public GridTiles<TileType>
-{
-  public:
-    RectGridBase(int64_t size_x, int64_t size_y) : GridTiles<TileType>(size_x, size_y),
-                                                   size_x_(size_x),
-                                                   size_y_(size_y)
-    {
+class RectGridBase : public GridTiles<TileType> {
+ public:
+  RectGridBase(int64_t size_x, int64_t size_y)
+      : GridTiles<TileType>(size_x, size_y), size_x_(size_x), size_y_(size_y) {}
+
+  virtual ~RectGridBase() = default;
+
+  int64_t SizeX() const { return size_x_; };
+  int64_t SizeY() const { return size_y_; };
+
+  // Note: resizing the grid may invalidate the reference to grid elements
+  //    acquired from GetTileRefAtRawCoordinate() and
+  //    GetTileRefAtGridCoordinate()
+  void ResizeGrid(int64_t x, int64_t y) {
+    if (x == size_x_ && y == size_y_) return;
+
+    assert(x > origin_offset_x_ && y > origin_offset_y_);
+
+    GridTiles<TileType>::ResizeGridTiles(x, y);
+    size_x_ = x;
+    size_y_ = y;
+  }
+
+  void SetOriginCoordinate(int64_t origin_x, int64_t origin_y) {
+    origin_offset_x_ = origin_x;
+    origin_offset_y_ = origin_y;
+  }
+
+  template <
+      typename T = TileType,
+      typename std::enable_if<std::is_floating_point<T>::value ||
+                              std::is_integral<T>::value>::type * = nullptr>
+  void PrintGrid() const {
+    for (int64_t y = 0; y < size_y_; ++y) {
+      for (int64_t x = 0; x < size_x_; ++x)
+        std::cout << std::setw(6)
+                  << GridTiles<TileType>::GetTileAtCoordinate(x, y);
+      std::cout << std::endl;
     }
+  }
 
-    virtual ~RectGridBase() = default;
+ protected:
+  // operations WRT coordinate of internal data structure directly
+  void SetTileAtRawCoordinate(int64_t x, int64_t y, TileType tile) {
+    assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
+    GridTiles<TileType>::SetTileAtCoordinate(x, y, tile);
+  }
 
-    int64_t SizeX() const { return size_x_; };
-    int64_t SizeY() const { return size_y_; };
+  TileType GetTileAtRawCoordinate(int64_t x, int64_t y) const {
+    assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
+    return GridTiles<TileType>::GetTileAtCoordinate(x, y);
+  }
 
-    // Note: resizing the grid may invalidate the reference to grid elements
-    //    acquired from GetTileRefAtRawCoordinate() and GetTileRefAtGridCoordinate()
-    void ResizeGrid(int64_t x, int64_t y)
-    {
-        if (x == size_x_ && y == size_y_)
-            return;
+  TileType &GetTileRefAtRawCoordinate(int64_t x, int64_t y) {
+    assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
+    return GridTiles<TileType>::GetTileRefAtCoordinate(x, y);
+  }
 
-        assert(x > origin_offset_x_ && y > origin_offset_y_);
+  // operations WRT coordinate of field
+  void SetTileAtGridCoordinate(int64_t x, int64_t y, TileType tile) {
+    auto internal_coordinate = ConvertToRawCoordinate(x, y);
+    SetTileAtRawCoordinate(internal_coordinate.GetX(),
+                           internal_coordinate.GetY(), tile);
+  }
 
-        GridTiles<TileType>::ResizeGridTiles(x, y);
-        size_x_ = x;
-        size_y_ = y;
-    }
+  TileType GetTileAtGridCoordinate(int64_t x, int64_t y) const {
+    auto internal_coordinate = ConvertToRawCoordinate(x, y);
+    return GetTileAtRawCoordinate(internal_coordinate.GetX(),
+                                  internal_coordinate.GetY());
+  }
 
-    void SetOriginCoordinate(int64_t origin_x, int64_t origin_y)
-    {
-        origin_offset_x_ = origin_x;
-        origin_offset_y_ = origin_y;
-    }
+  TileType &GetTileRefAtGridCoordinate(int64_t x, int64_t y) {
+    auto internal_coordinate = ConvertToRawCoordinate(x, y);
+    return GetTileRefAtRawCoordinate(internal_coordinate.GetX(),
+                                     internal_coordinate.GetY());
+  }
 
-    template <typename T = TileType, typename std::enable_if<std::is_floating_point<T>::value || std::is_integral<T>::value>::type * = nullptr>
-    void PrintGrid() const
-    {
-        for (int64_t y = 0; y < size_y_; ++y)
-        {
-            for (int64_t x = 0; x < size_x_; ++x)
-                std::cout << std::setw(6) << GridTiles<TileType>::GetTileAtCoordinate(x, y);
-            std::cout << std::endl;
-        }
-    }
+  // convertion between two coordinates
+  inline RectGridIndex ConvertToRawCoordinate(int64_t x, int64_t y) const {
+    return RectGridIndex(x + origin_offset_x_, y + origin_offset_y_);
+  }
 
-  protected:
-    // operations WRT coordinate of internal data structure directly
-    void SetTileAtRawCoordinate(int64_t x, int64_t y, TileType tile)
-    {
-        assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
+  inline RectGridIndex ConvertToGridCoordinate(int64_t x, int64_t y) const {
+    assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
+    return RectGridIndex(x - origin_offset_x_, y - origin_offset_y_);
+  }
 
-        GridTiles<TileType>::SetTileAtCoordinate(x, y, tile);
-    }
+ protected:
+  // internal data structure for a 2D field
+  int64_t size_x_ = 0;
+  int64_t size_y_ = 0;
 
-    TileType GetTileAtRawCoordinate(int64_t x, int64_t y) const
-    {
-        assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
-
-        return GridTiles<TileType>::GetTileAtCoordinate(x, y);
-    }
-
-    TileType &GetTileRefAtRawCoordinate(int64_t x, int64_t y)
-    {
-        assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
-
-        return GridTiles<TileType>::GetTileRefAtCoordinate(x, y);
-    }
-
-    // operations WRT coordinate of field
-    void SetTileAtGridCoordinate(int64_t x, int64_t y, TileType tile)
-    {
-        auto internal_coordinate = ConvertToRawCoordinate(x, y);
-
-        SetTileAtRawCoordinate(internal_coordinate.GetX(), internal_coordinate.GetY(), tile);
-    }
-
-    TileType GetTileAtGridCoordinate(int64_t x, int64_t y) const
-    {
-        auto internal_coordinate = ConvertToRawCoordinate(x, y);
-
-        return GetTileAtRawCoordinate(internal_coordinate.GetX(), internal_coordinate.GetY());
-    }
-
-    TileType &GetTileRefAtGridCoordinate(int64_t x, int64_t y)
-    {
-        auto internal_coordinate = ConvertToRawCoordinate(x, y);
-
-        return GetTileRefAtRawCoordinate(internal_coordinate.GetX(), internal_coordinate.GetY());
-    }
-
-    // convertion between two coordinates
-    inline RectGridIndex ConvertToRawCoordinate(int64_t x, int64_t y) const
-    {
-        return RectGridIndex(x + origin_offset_x_, y + origin_offset_y_);
-    }
-
-    inline RectGridIndex ConvertToGridCoordinate(int64_t x, int64_t y) const
-    {
-        assert((x >= 0) && (x < size_x_) && (y >= 0) && (y < size_y_));
-
-        return RectGridIndex(x - origin_offset_x_, y - origin_offset_y_);
-    }
-
-  protected:
-    // internal data structure for a 2D field
-    int64_t size_x_ = 0;
-    int64_t size_y_ = 0;
-
-    // origin offset
-    int64_t origin_offset_x_ = 0;
-    int64_t origin_offset_y_ = 0;
+  // origin offset
+  int64_t origin_offset_x_ = 0;
+  int64_t origin_offset_y_ = 0;
 };
-} // namespace robotnav
+}  // namespace robotnav
 
 #endif /* RECT_GRID_BASE_HPP */
--- src/modules/planning/decomp/include/decomp/details/square_grid_impl.hpp
+++ src/modules/planning/decomp/include/decomp/details/square_grid_impl.hpp
@@ -1,216 +1,199 @@
-/* 
+/*
  * square_grid_impl.hpp
- * 
+ *
  * Created on: Apr 08, 2018 18:01
- * Description: 
- * 
+ * Description:
+ *
  * Copyright (c) 2018 Ruixiang Du (rdu)
  */
 
 #ifndef SQUARE_GRID_IMPL_HPP
 #define SQUARE_GRID_IMPL_HPP
 
-namespace robotnav
-{
+namespace robotnav {
 template <typename T>
-SquareGridBase<T>::SquareGridBase(int32_t size_x, int32_t size_y, double cell_size) : RectGridBase<SquareCellBase<T> *>(size_x, size_y),
-                                                                                      cell_size_(cell_size)
-{
-    assert((size_x > 0 && size_y > 0));
+SquareGridBase<T>::SquareGridBase(int32_t size_x, int32_t size_y,
+                                  double cell_size)
+    : RectGridBase<SquareCellBase<T> *>(size_x, size_y), cell_size_(cell_size) {
+  assert((size_x > 0 && size_y > 0));
 
-    for (int32_t y = 0; y < size_y; y++)
-        for (int32_t x = 0; x < size_x; x++)
-        {
-            SquareCellBase<T> *new_cell = new SquareCellBase<T>(x, y, IndexToID(x, y));
-            new_cell->UpdateGeometry(cell_size);
-            RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(x, y, new_cell);
-        }
-}
-
-template <typename T>
-SquareGridBase<T>::SquareGridBase(const Eigen::MatrixXd &matrix, int32_t side_length, double cell_size) : RectGridBase<SquareCellBase<T> *>(0, 0),
-                                                                                                          cell_size_(cell_size)
-{
-    int32_t grid_size_x, grid_size_y;
-    Eigen::MatrixXd occupancy_matrix;
-
-    if (side_length == 1)
-    {
-        grid_size_x = matrix.cols();
-        grid_size_y = matrix.rows();
-        occupancy_matrix = matrix;
+  for (int32_t y = 0; y < size_y; y++)
+    for (int32_t x = 0; x < size_x; x++) {
+      SquareCellBase<T> *new_cell =
+          new SquareCellBase<T>(x, y, IndexToID(x, y));
+      new_cell->UpdateGeometry(cell_size);
+      RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(x, y, new_cell);
     }
-    else
-    {
-        bool shrink_x = false;
-        bool shrink_y = false;
+}
 
-        // determine size of grid
-        int32_t center_x = matrix.cols() / 2;
-        int32_t center_y = matrix.rows() / 2;
+template <typename T>
+SquareGridBase<T>::SquareGridBase(const Eigen::MatrixXd &matrix,
+                                  int32_t side_length, double cell_size)
+    : RectGridBase<SquareCellBase<T> *>(0, 0), cell_size_(cell_size) {
+  int32_t grid_size_x, grid_size_y;
+  Eigen::MatrixXd occupancy_matrix;
 
-        if (matrix.cols() % side_length != 0)
-            shrink_x = true;
-        if (matrix.rows() % side_length != 0)
-            shrink_y = true;
-        grid_size_x = (center_x / side_length) * 2;
-        grid_size_y = (center_y / side_length) * 2;
+  if (side_length == 1) {
+    grid_size_x = matrix.cols();
+    grid_size_y = matrix.rows();
+    occupancy_matrix = matrix;
+  } else {
+    bool shrink_x = false;
+    bool shrink_y = false;
 
-        if (shrink_x || shrink_y)
-        {
-            occupancy_matrix = Eigen::MatrixXd::Ones(grid_size_y * side_length, grid_size_x * side_length);
+    // determine size of grid
+    int32_t center_x = matrix.cols() / 2;
+    int32_t center_y = matrix.rows() / 2;
 
-            int32_t x_start = 0, y_start = 0;
-            if (shrink_x)
-                x_start = center_x % side_length;
-            if (shrink_y)
-                y_start = center_y % side_length;
+    if (matrix.cols() % side_length != 0) shrink_x = true;
+    if (matrix.rows() % side_length != 0) shrink_y = true;
+    grid_size_x = (center_x / side_length) * 2;
+    grid_size_y = (center_y / side_length) * 2;
 
-            occupancy_matrix = matrix.block(y_start, x_start, occupancy_matrix.rows(), occupancy_matrix.cols());
-        }
-        else
-        {
-            occupancy_matrix = matrix;
-        }
+    if (shrink_x || shrink_y) {
+      occupancy_matrix = Eigen::MatrixXd::Ones(grid_size_y * side_length,
+                                               grid_size_x * side_length);
+
+      int32_t x_start = 0, y_start = 0;
+      if (shrink_x) x_start = center_x % side_length;
+      if (shrink_y) y_start = center_y % side_length;
+
+      occupancy_matrix = matrix.block(y_start, x_start, occupancy_matrix.rows(),
+                                      occupancy_matrix.cols());
+    } else {
+      occupancy_matrix = matrix;
     }
+  }
 
-    // create new grid
-    RectGridBase<SquareCellBase<T> *>::ResizeGrid(grid_size_x, grid_size_y);
-    for (int32_t y = 0; y < grid_size_y; y++)
-        for (int32_t x = 0; x < grid_size_x; x++)
-        {
-            SquareCellBase<T> *new_cell = new SquareCellBase<T>(x, y, IndexToID(x, y));
-            new_cell->UpdateGeometry(cell_size);
-            RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(x, y, new_cell);
-        }
-
-    // determine occupancy of grid
-    int32_t half_size_x = grid_size_x / 2;
-    int32_t half_size_y = grid_size_y / 2;
-    for (int64_t x = 0; x < RectGridBase<SquareCellBase<T> *>::SizeX(); ++x)
-        for (int64_t y = 0; y < RectGridBase<SquareCellBase<T> *>::SizeY(); ++y)
-        {
-            bool occupied = false;
-            int32_t xmin = x * side_length;
-            int32_t xmax = xmin + side_length;
-            int32_t ymin = y * side_length;
-            int32_t ymax = ymin + side_length;
-            for (int i = xmin; i < xmax; ++i)
-            {
-                for (int j = ymin; j < ymax; ++j)
-                {
-                    if (occupancy_matrix(j, i) != 0)
-                    {
-                        SetCellLabel(x, y, SquareCellLabel::OCCUPIED);
-                        occupied = true;
-                        break;
-                    }
-                }
-                if (occupied)
-                    break;
-            }
-        }
-}
-
-template <typename T>
-SquareGridBase<T>::~SquareGridBase()
-{
-    for (int32_t y = 0; y < RectGridBase<SquareCellBase<T> *>::size_y_; y++)
-        for (int32_t x = 0; x < RectGridBase<SquareCellBase<T> *>::size_x_; x++)
-            delete RectGridBase<SquareCellBase<T> *>::GetTileAtRawCoordinate(x, y);
-}
-
-template <typename T>
-SquareGridBase<T>::SquareGridBase(const SquareGridBase<T> &other) : RectGridBase<SquareCellBase<T> *>(other.SizeX(), other.SizeY()),
-                                                                    cell_size_(other.cell_size_)
-{
-    for (auto row : other.grid_tiles_)
-        for (auto cell : row)
-            RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(cell->x, cell->y, new SquareCellBase<T>(*cell));
-}
-
-template <typename T>
-SquareGridBase<T> &SquareGridBase<T>::operator=(const SquareGridBase<T> &other)
-{
-    SquareGridBase<T> tmp(other);
-    *this = std::move(tmp);
-    return *this;
-}
-
-template <typename T>
-SquareGridBase<T>::SquareGridBase(SquareGridBase<T> &&other) : RectGridBase<SquareCellBase<T> *>(other.SizeX(), other.SizeY()),
-                                                               cell_size_(other.cell_size_)
-{
-    for (auto row : other.grid_tiles_)
-        for (auto cell : row)
-            RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(cell->x, cell->y, cell);
-}
-
-template <typename T>
-SquareGridBase<T> &SquareGridBase<T>::operator=(SquareGridBase<T> &&other)
-{
-    RectGridBase<SquareCellBase<T> *>::ResizeGridTiles(other.size_x, other.size_y);
-    cell_size_ = std::move(other.cell_size);
-    for (auto row : other.grid_tiles_)
-        for (auto cell : row)
-            RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(cell->x, cell->y, cell);
-
-    return *this;
-}
-
-template <typename T>
-SquareCellBase<T> *SquareGridBase<T>::GetCell(int64_t id)
-{
-    auto coordinate = IDToIndex(id);
-    return RectGridBase<SquareCellBase<T> *>::GetTileAtGridCoordinate(coordinate.GetX(), coordinate.GetY());
-}
-
-template <typename T>
-SquareCellBase<T> *SquareGridBase<T>::GetCell(int32_t x, int32_t y)
-{
-    return RectGridBase<SquareCellBase<T> *>::GetTileAtGridCoordinate(x, y);
-}
-
-template <typename T>
-std::vector<SquareCellBase<T> *> SquareGridBase<T>::GetNeighbours(int32_t x, int32_t y, bool allow_diag)
-{
-    std::vector<RectGridIndex> candidates;
-    if (allow_diag)
-    {
-        for (int32_t xi = x - 1; xi <= x + 1; ++xi)
-            for (int32_t yi = y - 1; yi <= y + 1; ++yi)
-            {
-                if (xi == x && yi == y)
-                    continue;
-                candidates.emplace_back(xi, yi);
-            }
-    }
-    else
-    {
-        candidates.emplace_back(x, y + 1);
-        candidates.emplace_back(x, y - 1);
-        candidates.emplace_back(x + 1, y);
-        candidates.emplace_back(x - 1, y);
+  // create new grid
+  RectGridBase<SquareCellBase<T> *>::ResizeGrid(grid_size_x, grid_size_y);
+  for (int32_t y = 0; y < grid_size_y; y++)
+    for (int32_t x = 0; x < grid_size_x; x++) {
+      SquareCellBase<T> *new_cell =
+          new SquareCellBase<T>(x, y, IndexToID(x, y));
+      new_cell->UpdateGeometry(cell_size);
+      RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(x, y, new_cell);
     }
 
-    std::vector<SquareCellBase<T> *> neighbours;
-    for (auto &can : candidates)
-    {
-        int32_t xi = can.GetX();
-        int32_t yi = can.GetY();
-        if (xi >= 0 && xi < RectGridBase<SquareCellBase<T> *>::size_x_ && yi >= 0 && yi < RectGridBase<SquareCellBase<T> *>::size_y_)
-            neighbours.push_back(RectGridBase<SquareCellBase<T> *>::GetTileAtRawCoordinate(xi, yi));
+  // determine occupancy of grid
+  int32_t half_size_x = grid_size_x / 2;
+  int32_t half_size_y = grid_size_y / 2;
+  for (int64_t x = 0; x < RectGridBase<SquareCellBase<T> *>::SizeX(); ++x)
+    for (int64_t y = 0; y < RectGridBase<SquareCellBase<T> *>::SizeY(); ++y) {
+      bool occupied = false;
+      int32_t xmin = x * side_length;
+      int32_t xmax = xmin + side_length;
+      int32_t ymin = y * side_length;
+      int32_t ymax = ymin + side_length;
+      for (int i = xmin; i < xmax; ++i) {
+        for (int j = ymin; j < ymax; ++j) {
+          if (occupancy_matrix(j, i) != 0) {
+            SetCellLabel(x, y, SquareCellLabel::OCCUPIED);
+            occupied = true;
+            break;
+          }
+        }
+        if (occupied) break;
+      }
     }
-
-    return neighbours;
 }
 
 template <typename T>
-std::vector<SquareCellBase<T> *> SquareGridBase<T>::GetNeighbours(int64_t id, bool allow_diag)
-{
-    auto index = IDToIndex(id);
-    return GetNeighbours(index.GetX(), index.GetY(), allow_diag);
+SquareGridBase<T>::~SquareGridBase() {
+  for (int32_t y = 0; y < RectGridBase<SquareCellBase<T> *>::size_y_; y++)
+    for (int32_t x = 0; x < RectGridBase<SquareCellBase<T> *>::size_x_; x++)
+      delete RectGridBase<SquareCellBase<T> *>::GetTileAtRawCoordinate(x, y);
 }
-} // namespace robotnav
+
+template <typename T>
+SquareGridBase<T>::SquareGridBase(const SquareGridBase<T> &other)
+    : RectGridBase<SquareCellBase<T> *>(other.SizeX(), other.SizeY()),
+      cell_size_(other.cell_size_) {
+  for (auto row : other.grid_tiles_)
+    for (auto cell : row)
+      RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(
+          cell->x, cell->y, new SquareCellBase<T>(*cell));
+}
+
+template <typename T>
+SquareGridBase<T> &SquareGridBase<T>::operator=(
+    const SquareGridBase<T> &other) {
+  SquareGridBase<T> tmp(other);
+  *this = std::move(tmp);
+  return *this;
+}
+
+template <typename T>
+SquareGridBase<T>::SquareGridBase(SquareGridBase<T> &&other)
+    : RectGridBase<SquareCellBase<T> *>(other.SizeX(), other.SizeY()),
+      cell_size_(other.cell_size_) {
+  for (auto row : other.grid_tiles_)
+    for (auto cell : row)
+      RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(cell->x,
+                                                                cell->y, cell);
+}
+
+template <typename T>
+SquareGridBase<T> &SquareGridBase<T>::operator=(SquareGridBase<T> &&other) {
+  RectGridBase<SquareCellBase<T> *>::ResizeGridTiles(other.size_x,
+                                                     other.size_y);
+  cell_size_ = std::move(other.cell_size);
+  for (auto row : other.grid_tiles_)
+    for (auto cell : row)
+      RectGridBase<SquareCellBase<T> *>::SetTileAtRawCoordinate(cell->x,
+                                                                cell->y, cell);
+
+  return *this;
+}
+
+template <typename T>
+SquareCellBase<T> *SquareGridBase<T>::GetCell(int64_t id) {
+  auto coordinate = IDToIndex(id);
+  return RectGridBase<SquareCellBase<T> *>::GetTileAtGridCoordinate(
+      coordinate.GetX(), coordinate.GetY());
+}
+
+template <typename T>
+SquareCellBase<T> *SquareGridBase<T>::GetCell(int32_t x, int32_t y) {
+  return RectGridBase<SquareCellBase<T> *>::GetTileAtGridCoordinate(x, y);
+}
+
+template <typename T>
+std::vector<SquareCellBase<T> *> SquareGridBase<T>::GetNeighbours(
+    int32_t x, int32_t y, bool allow_diag) {
+  std::vector<RectGridIndex> candidates;
+  if (allow_diag) {
+    for (int32_t xi = x - 1; xi <= x + 1; ++xi)
+      for (int32_t yi = y - 1; yi <= y + 1; ++yi) {
+        if (xi == x && yi == y) continue;
+        candidates.emplace_back(xi, yi);
+      }
+  } else {
+    candidates.emplace_back(x, y + 1);
+    candidates.emplace_back(x, y - 1);
+    candidates.emplace_back(x + 1, y);
+    candidates.emplace_back(x - 1, y);
+  }
+
+  std::vector<SquareCellBase<T> *> neighbours;
+  for (auto &can : candidates) {
+    int32_t xi = can.GetX();
+    int32_t yi = can.GetY();
+    if (xi >= 0 && xi < RectGridBase<SquareCellBase<T> *>::size_x_ && yi >= 0 &&
+        yi < RectGridBase<SquareCellBase<T> *>::size_y_)
+      neighbours.push_back(
+          RectGridBase<SquareCellBase<T> *>::GetTileAtRawCoordinate(xi, yi));
+  }
+
+  return neighbours;
+}
+
+template <typename T>
+std::vector<SquareCellBase<T> *> SquareGridBase<T>::GetNeighbours(
+    int64_t id, bool allow_diag) {
+  auto index = IDToIndex(id);
+  return GetNeighbours(index.GetX(), index.GetY(), allow_diag);
+}
+}  // namespace robotnav
 
 #endif /* SQUARE_GRID_IMPL_HPP */
