fb36ac4ae9372f027c41e7be526ac1e72f094051
Ruixiang Du <ruixiang.du@gmail.com>
2021-01-28 22:16:51 +0800

    saved work on map

---
+++ src/modules/mapping/map/test/test_pgm_map.cpp
@@ -0,0 +1,16 @@
+/* 
+ * test_pgm_map.cpp
+ * 
+ * Created on: Jan 28, 2021 22:01
+ * Description: 
+ * 
+ * Copyright (c) 2021 Ruixiang Du (rdu)
+ */ 
+
+#include "map/pgm_map.hpp"
+
+using namespace robotnav;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
