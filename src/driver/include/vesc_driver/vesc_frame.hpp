5250ce246890f8fe2eecdef2a77e329f905b9017
Ruixiang Du <ruixiang.du@gmail.com>
2022-07-03 23:18:16 +0800

    cmake: updated installation configuration for driver module

--- src/driver/include/vesc_driver/vesc_frame.hpp
+++ src/driver/include/vesc_driver/vesc_frame.hpp
@@ -4,6 +4,11 @@
  * Created on 7/1/22 9:01 PM
  * Description:
  *
+ * Reference:
+ * [1] https://dongilc.gitbook.io/openrobot-inc/tutorials/control-with-can
+ * [2] comm/comm_can.c in https://github.com/vedderb/bldc.git
+ * [3] datatypes.h in https://github.com/vedderb/bldc.git
+ *
  * Copyright (c) 2022 Ruixiang Du (rdu)
  */
 
