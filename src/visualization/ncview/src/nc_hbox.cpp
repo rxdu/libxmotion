97bfcb0019d527ae52fed01ca90be01166c2c690
Ruixiang Du <ruixiang.du@gmail.com>
2023-02-19 17:18:10 +0800

    nc_vbot/nc_hbot: assign unallocated space to last element

--- src/visualization/ncview/src/nc_hbox.cpp
+++ src/visualization/ncview/src/nc_hbox.cpp
@@ -14,17 +14,22 @@ namespace robosw {
 namespace swviz {
 void NcHbox::AllocateSpace(int rows, int cols) {
   float allocated_ratio = 0;
+  int allocated_cols = 0;
   for (int i = 0; i < components_.size(); ++i) {
     if (components_[i].constraint.type == NcConstraint::Type::kFixed) {
       allocated_sizes_[components_[i].element] = components_[i].constraint.ratio * cols;
       allocated_ratio += components_[i].constraint.ratio;
+      allocated_cols += allocated_sizes_[components_[i].element];
     }
   }
   for (int i = 0; i < components_.size(); ++i) {
     if (components_[i].constraint.type == NcConstraint::Type::kFlexible) {
       allocated_sizes_[components_[i].element] = (1 - allocated_ratio) / num_of_flex_components_ * cols;
+      allocated_cols += allocated_sizes_[components_[i].element];
     }
   }
+  // assign the remaining space to the last element
+  if (allocated_cols < cols) { allocated_sizes_[components_.back().element] += cols - allocated_cols; }
 }
 
 void NcHbox::OnResize(int rows, int cols, int y, int x) {
