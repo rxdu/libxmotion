76abd7566a2f5affadfc3536ddb972cd41fd2af7
Ruixiang Du <ruixiang.du@gmail.com>
2023-05-31 23:29:51 +0800

    joystick: added joystick interface, working on evdev-based js driver for Linux

---
+++ src/driver/input_joystick/include/input_joystick/joystick.hpp
@@ -0,0 +1,20 @@
+/*
+ * joystick.hpp
+ *
+ * Created on 5/30/23 11:00 PM
+ * Description:
+ *
+ * Reference: https://github.com/MysteriousJ/Joystick-Input-Examples
+ *
+ * Copyright (c) 2023 Ruixiang Du (rdu)
+ */
+
+#ifndef ROBOSW_JOYSTICK_HPP
+#define ROBOSW_JOYSTICK_HPP
+
+class Joystick {
+ public:
+ private:
+};
+
+#endif  // ROBOSW_JOYSTICK_HPP
---
+++ src/driver/input_joystick/src/joystick.cpp
@@ -0,0 +1,12 @@
+/*
+ * joystick.cpp
+ *
+ * Created on 5/30/23 11:00 PM
+ * Description:
+ *
+ * Copyright (c) 2023 Ruixiang Du (rdu)
+ */
+
+#include "input_joystick/joystick.hpp"
+
+namespace {}
