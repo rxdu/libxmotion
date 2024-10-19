15e0c6d67a4d13e807968bb077a0203410fc22d5
Ruixiang Du <ruixiang.du@gmail.com>
2024-10-19 10:29:01 +0800

    waveshare: servo driver from vendor works

--- src/driver/motor_waveshare/test/examples/SMS_STS/Ping/Ping.cpp
+++ src/driver/motor_waveshare/test/examples/SMS_STS/Ping/Ping.cpp
@@ -3,27 +3,34 @@ Ping指令测试,测试总线上相应ID舵机是否就绪,广播指令只适用
 */
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "SCServo.h"
 
 SMS_STS sm_st;
 
-int main(int argc, char **argv)
-{
-	if(argc<2){
-        std::cout<<"argc error!"<<std::endl;
-        return 0;
-	}
-	std::cout<<"serial:"<<argv[1]<<std::endl;
-    if(!sm_st.begin(115200, argv[1])){
-        std::cout<<"Failed to init sms/sts motor!"<<std::endl;
-        return 0;
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cout << "argc error!" << std::endl;
+    return 0;
+  }
+  std::cout << "serial:" << argv[1] << std::endl;
+  if (!sm_st.begin(1000000, argv[1])) {
+    std::cout << "Failed to init sms/sts motor!" << std::endl;
+    return 0;
+  }
+
+  for (int i = 0; i < 255; i++) {
+    int ID = sm_st.Ping(i);
+    if (ID != -1) {
+      std::cout << "ID:" << ID << std::endl;
+      break;
+    } else {
+      std::cout << "Ping servo ID error: " << i << " !" << std::endl;
     }
-	int ID = sm_st.Ping(1);
-	if(ID!=-1){
-		std::cout<<"ID:"<<ID<<std::endl;
-	}else{
-		std::cout<<"Ping servo ID error!"<<std::endl;
-	}
-	sm_st.end();
-	return 1;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  sm_st.end();
+  return 1;
 }
