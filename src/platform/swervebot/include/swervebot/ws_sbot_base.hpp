dc5065e7d475494d22ecd8b5441aaa63cbdfaa07
Ruixiang Du <ruixiang.du@gmail.com>
2024-11-13 22:57:01 +0800

    swervebot: added sbot class

--- src/platform/swervebot/include/swervebot/ws_sbot_base.hpp
+++ src/platform/swervebot/include/swervebot/ws_sbot_base.hpp
@@ -9,11 +9,28 @@
 #ifndef XMOTION_WS_SBOT_BASE_HPP
 #define XMOTION_WS_SBOT_BASE_HPP
 
+#include "motor_waveshare/ddsm_210_array.hpp"
+#include "motor_waveshare/sms_sts_servo_array.hpp"
 #include "robot_base/swerve_drive_robot.hpp"
 
 namespace xmotion {
-class WsSbotBase : public SwerveDriveRobot {
+class WsSbotBase {
  public:
+  using ModelConfig = SwerveDriveRobot::Config;
+
+ public:
+  WsSbotBase() {}
+
+  bool Initialize();
+
+ private:
+  static constexpr float sbot_track_width = 0.198;
+  static constexpr float sbot_wheel_base = 0.245;
+  static constexpr float sbot_wheel_radius = 0.0363;
+
+  std::shared_ptr<SmsStsServoArray> steering_motor_;
+  std::shared_ptr<Ddsm210Array> driving_motor_;
+  std::unique_ptr<SwerveDriveRobot> robot_;
 };
 }  // namespace xmotion
 
--- src/platform/swervebot/src/ws_sbot_base.cpp
+++ src/platform/swervebot/src/ws_sbot_base.cpp
@@ -8,4 +8,40 @@
 
 #include "swervebot/ws_sbot_base.hpp"
 
-namespace xmotion {}  // namespace xmotion
+namespace xmotion {
+bool WsSbotBase::Initialize() {
+  steering_motor_ = std::make_shared<SmsStsServoArray>("/dev/ttyUSB0");
+  steering_motor_->SetPositionOffset(180);
+  steering_motor_->SetDefaultPosition(0);
+  steering_motor_->RegisterMotor(1);
+  steering_motor_->RegisterMotor(2);
+  steering_motor_->RegisterMotor(3);
+  steering_motor_->RegisterMotor(4);
+  if (!steering_motor_->Connect()) {
+    std::cout << "Failed to connect to steering motor" << std::endl;
+    return false;
+  }
+
+  // set up driving motors
+  driving_motor_ = std::make_shared<Ddsm210Array>("/dev/ttyUSB1");
+  driving_motor_->RegisterMotor(1);
+  driving_motor_->RegisterMotor(2);
+  driving_motor_->RegisterMotor(3);
+  driving_motor_->RegisterMotor(4);
+  if (!driving_motor_->Connect()) {
+    std::cout << "Failed to connect to driving motor" << std::endl;
+    return false;
+  }
+
+  SwerveDriveRobot::Config config;
+  config.track_width = sbot_track_width;
+  config.wheel_base = sbot_wheel_base;
+  config.wheel_radius = sbot_wheel_radius;
+  config.steering_motors = steering_motor_;
+  config.driving_motors = driving_motor_;
+  config.reverse_right_wheels = true;
+  robot_ = std::make_unique<SwerveDriveRobot>(config);
+
+  return true;
+}
+}  // namespace xmotion
